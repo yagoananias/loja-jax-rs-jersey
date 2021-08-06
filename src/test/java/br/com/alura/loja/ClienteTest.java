ee17de8f9a627cc875540a654f72a5686e773a31
Yago Ananias <yago_souza92@yahoo.com.br>
2021-08-05 23:33:09 -0300

    Teste end-to-end

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,20 +4,39 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
 import junit.framework.Assert;
 
 public class ClienteTest {
 	
+	private HttpServer server;
+
+	@Before
+	public void startaServidor() {
+		server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void derrubaServidor() {
+		server.stop();
+	}
+	
 	@Test
-	public void testaConexao() {
+	public void testaBuscarCarrinhoEsperado() {		
 		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("https://run.mocky.io");
-		String conteudo = target.path("/v3/987fc2f2-b801-4281-a886-80a15fc8e77a").request().get(String.class);
-		System.out.println(conteudo);
-		Assert.assertTrue(conteudo.contains("message"));
+		WebTarget target = client.target("http://localhost:3000");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+		
 		
 	}
 
--- src/test/java/br/com/alura/loja/Servidor.java
+++ src/test/java/br/com/alura/loja/Servidor.java
@@ -1,6 +1,5 @@
 package br.com.alura.loja;
 
-import java.io.IOException;
 import java.net.URI;
 
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -9,15 +8,17 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Servidor {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
+		
+		inicializaServidor();		
+
+	}
+
+	static HttpServer inicializaServidor() {
 		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
 		URI uri = URI.create("http://localhost:3000/");
 		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
-		System.out.println("Rodando!");
-		System.in.read();
-		server.stop();
-		
-
+		return server;
 	}
 
 }
